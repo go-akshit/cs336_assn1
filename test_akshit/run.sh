0092bf6ca9a2a203222333cbab364b3796a44afa
akshit317 <akshitgoel4@gmail.com>
2024-04-13 20:25:57 -0700

    2.6.2

--- test_akshit/run.sh
+++ test_akshit/run.sh
@@ -8,5 +8,5 @@
 #SBATCH --error=encode_iterable_memory_usage_%j.err
 #SBATCH --mem=50G
 #python3 train_bpe_datasets.py
-../pytest -k test_encode_iterable_memory_usage
+pytest -k test_encode_iterable_memory_usage
 python3 train_bpe_datasets.py
