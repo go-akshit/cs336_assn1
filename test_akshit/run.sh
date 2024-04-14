70ff48e820586be50eec77bf6e6f32cc971cc952
akshit317 <akshitgoel4@gmail.com>
2024-04-13 19:19:38 -0700

    try

--- test_akshit/run.sh
+++ test_akshit/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=bpe_train
+#SBATCH --job-name=bpe_train_tiny
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:05:00
-#SBATCH --output=bpe_train_%j.out
-#SBATCH --error=bpe_train_%j.err
+#SBATCH --time=00:60:00
+#SBATCH --output=bpe_train_tiny_%j.out
+#SBATCH --error=bpe_train_tiny_%j.err
+#SBATCH --mem=50G
 python3 train_bpe_datasets.py
